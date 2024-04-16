20dd538285c0ffc730c2f91b2ab66ba5f2b5b65c
Lakshitha <notlakshitha@gmail.com>
2024-04-16 11:54:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,188 @@
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const cors = require("cors");
+
+const port = 3000;
+const USER_HEADER = `//later`;
+
+const app = express();
+app.use(cors());
+
+app.get("/get", async (req, res) => {
+  try {
+    const kawaiilinks = `https://kawaiifu.com/`;
+    const linkreq = await axios.get(kawaiilinks);
+    const linkkawa = linkreq.data;
+
+    const $ = cheerio.load(linkkawa);
+
+    const mostwatched = [];
+
+    const ongoing = [];
+
+    const spring = [];
+
+    const movies = [];
+
+    const tvseries = [];
+
+    const dub = [];
+
+    const music = [];
+
+    $(".list-film:first")
+      .find(".item")
+      .each((index, element) => {
+        const name = $(element).find("a:eq(2)").text() || null;
+        const jname = $(element).find("a:eq(1)").text() || null;
+        const link = $(element).find("a").attr("href") || null;
+        const view = $(element).find(".view").text().split('g:')[1].trim() || null;
+        const image = $(element).find("img").attr("src") || null;
+        const decrip = $(element).find(".cot1").text().trim() || null;
+        const id = link.split(".com")[1];
+        const release = $(element).find(".vl-chil-date:first").text() || null;
+        const genre = $(element).find(".vl-chil-date:eq(1)").map((index, element)=>
+           ( $(element).find("a").text())).get();
+        const director = $(element).find(".vl-chil-date:eq(2)").text() || null;
+        const rating = $(element).find(".fl-left").text().split('g:')[1].trim().split('(')[0].trim() || null;
+
+
+        mostwatched.push({ name, jname, link, view, image, decrip,id,release , genre, director, rating});
+      });
+
+      $(".list-film:eq(1)")
+      .find(".item")
+      .each((index, element) => {
+        const name = $(element).find("a:eq(2)").text() || null;
+        const jname = $(element).find("a:eq(1)").text() || null;
+        const link = $(element).find("a").attr("href") || null;
+        const view = $(element).find(".view").text().split('g:')[1].trim() || null;
+        const image = $(element).find("img").attr("src") || null;
+        const decrip = $(element).find(".cot1").text().trim() || null;
+        const id = link.split(".com")[1];
+        const release = $(element).find(".vl-chil-date:first").text() || null;
+        const genre = $(element).find(".vl-chil-date:eq(1)").map((index, element)=>
+           ( $(element).find("a").text())).get();
+        const director = $(element).find(".vl-chil-date:eq(2)").text() || null;
+        const rating = $(element).find(".fl-left").text().split('g:')[1].trim().split('(')[0].trim() || null;
+
+
+
+        ongoing.push({ name, jname, link, view, image, decrip,id,release , genre, director, rating});
+      });
+
+      $(".list-film:eq(2)")
+      .find(".item")
+      .each((index, element) => {
+        const name = $(element).find("a:eq(2)").text() || null;
+        const jname = $(element).find("a:eq(1)").text() || null;
+        const link = $(element).find("a").attr("href") || null;
+        const view = $(element).find(".view").text().split('g:')[1].trim() || null;
+        const image = $(element).find("img").attr("src") || null;
+        const decrip = $(element).find(".cot1").text().trim() || null;
+        const id = link.split(".com")[1];
+        const release = $(element).find(".vl-chil-date:first").text() || null;
+        const genre = $(element).find(".vl-chil-date:eq(1)").map((index, element)=>
+           ( $(element).find("a").text())).get();
+        const director = $(element).find(".vl-chil-date:eq(2)").text() || null;
+        const rating = $(element).find(".fl-left").text().split('g:')[1].trim().split('(')[0].trim() || null;
+
+
+
+        spring.push({ name, jname, link, view, image, decrip,id,release , genre, director, rating});
+      });
+
+      $(".list-film:eq(3)")
+      .find(".item")
+      .each((index, element) => {
+        const name = $(element).find("a:eq(2)").text() || null;
+        const jname = $(element).find("a:eq(1)").text() || null;
+        const link = $(element).find("a").attr("href") || null;
+        const view = $(element).find(".view").text().split('g:')[1].trim() || null;
+        const image = $(element).find("img").attr("src") || null;
+        const decrip = $(element).find(".cot1").text().trim() || null;
+        const id = link.split(".com")[1];
+        const release = $(element).find(".vl-chil-date:first").text() || null;
+        const genre = $(element).find(".vl-chil-date:eq(1)").map((index, element)=>
+           ( $(element).find("a").text())).get();
+        const director = $(element).find(".vl-chil-date:eq(2)").text() || null;
+        const rating = $(element).find(".fl-left").text().split('g:')[1].trim().split('(')[0].trim() || null;
+
+
+
+        movies.push({ name, jname, link, view, image, decrip,id,release , genre, director, rating});
+      });
+
+      $(".list-film:eq(4)")
+      .find(".item")
+      .each((index, element) => {
+        const name = $(element).find("a:eq(2)").text() || null;
+        const jname = $(element).find("a:eq(1)").text() || null;
+        const link = $(element).find("a").attr("href") || null;
+        const view = $(element).find(".view").text().split('g:')[1].trim() || null;
+        const image = $(element).find("img").attr("src") || null;
+        const decrip = $(element).find(".cot1").text().trim() || null;
+        const id = link.split(".com")[1];
+        const release = $(element).find(".vl-chil-date:first").text() || null;
+        const genre = $(element).find(".vl-chil-date:eq(1)").map((index, element)=>
+           ( $(element).find("a").text())).get();
+        const director = $(element).find(".vl-chil-date:eq(2)").text() || null;
+        const rating = $(element).find(".fl-left").text().split('g:')[1].trim().split('(')[0].trim() || null;
+
+
+
+        tvseries.push({ name, jname, link, view, image, decrip,id,release , genre, director, rating});
+      });
+
+      $(".list-film:eq(5)")
+      .find(".item")
+      .each((index, element) => {
+        const name = $(element).find("a:eq(2)").text() || null;
+        const jname = $(element).find("a:eq(1)").text() || null;
+        const link = $(element).find("a").attr("href") || null;
+        const view = $(element).find(".view").text().split('g:')[1].trim() || null;
+        const image = $(element).find("img").attr("src") || null;
+        const decrip = $(element).find(".cot1").text().trim() || null;
+        const id = link.split(".com")[1];
+        const release = $(element).find(".vl-chil-date:first").text() || null;
+        const genre = $(element).find(".vl-chil-date:eq(1)").map((index, element)=>
+           ( $(element).find("a").text())).get();
+        const director = $(element).find(".vl-chil-date:eq(2)").text() || null;
+        const rating = $(element).find(".fl-left").text().split('g:')[1].trim().split('(')[0].trim() || null;
+
+
+
+        dub.push({ name, jname, link, view, image, decrip,id,release , genre, director, rating});
+      });
+
+      $(".list-film:eq(6)")
+      .find(".item")
+      .each((index, element) => {
+        const name = $(element).find("a:eq(2)").text() || null;
+        const jname = $(element).find("a:eq(1)").text() || null;
+        const link = $(element).find("a").attr("href") || null;
+        const view = $(element).find(".view").text().split('g:')[1].trim() || null;
+        const image = $(element).find("img").attr("src") || null;
+        const decrip = $(element).find(".cot1").text().trim() || null;
+        const id = link.split(".com")[1];
+        const release = $(element).find(".vl-chil-date:first").text() || null;
+        const genre = $(element).find(".vl-chil-date:eq(1)").map((index, element)=>
+           ( $(element).find("a").text())).get();
+        const director = $(element).find(".vl-chil-date:eq(2)").text() || null;
+        const rating = $(element).find(".fl-left").text().split('g:')[1].trim().split('(')[0].trim() || null;
+
+
+
+        music.push({ name, jname, link, view, image, decrip,id,release , genre, director, rating});
+      });
+
+    res.json({mostwatched,ongoing, spring, movies, tvseries, dub, music});
+  } catch (e) {
+    console.log("error: ", e);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
